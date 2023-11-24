a3054c22841aee1eaf48d026fc5537d67d91fdbb
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-11-24 05:49:31 -0300

    feita a pagina benefícios

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ import PaginaInicial from '../src/paginaInicial/PaginaInicial';
 import PaginaCartãoBlack from './paginaCartãoBlack/PaginaCartãoBlack';
 import PaginaCartãoStandart from './paginaCartãoStandart/PaginaCartãoStandart';
 function App() {
-  const [informações, Setinformações] = useState([])
-
-  function onAddInformações(nome, email) {
-    let info = new Informações(nome, email)
-    Setinformações([...informações, info])
-  }
 
   return (
     <div>
-      <PaginaInicial className="w-screen h-[1024] bg-roxo" ></PaginaInicial >
-      <PaginaCartãoBlack className="w-[1440px] h-[1024px]"></PaginaCartãoBlack>
+      {/* <PaginaInicial className="w-screen h-[1024] bg-roxo" ></PaginaInicial >
+      <PaginaCartãoBlack className="w-[1440px] h-[1024px]"></PaginaCartãoBlack> */}
       <PaginaCartãoStandart className="w-[1440px] h-[1024px]"></PaginaCartãoStandart>
     </div >
   );
