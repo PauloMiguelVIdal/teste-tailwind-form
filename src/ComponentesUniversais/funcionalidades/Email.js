29cde963a3e7642d4fe9974b65047784d8c26ca1
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-12-26 07:57:13 -0300

    testando mudanaças de página no cartão

--- src/ComponentesUniversais/funcionalidades/Email.js
+++ src/ComponentesUniversais/funcionalidades/Email.js
@@ -13,20 +13,19 @@ setNovoEmail(t)
 if(novoEmail){
     AtualizarDadosEmail(novoEmail)
 }
-}
 
     return (
         <div>
-            <input
+            {/* <input
                 type="text"
                 onChange={handleChangeEmail}
                 value={novoEmail}
                 placeholder="Email"
                 className="placeholder:text-white placeholder:opacity-70 z-50 text-[25px] fonteBold w-[470px] pl-[10px] h-[60px] bg-roxo bg-opacity-20 rounded-[17.50px]"
-            />
+            /> */}
         </div>
     );
-
+}
 
 
 
--- src/ComponentesUniversais/modelosfuncionalidades/NomeEmail.js
+++ src/ComponentesUniversais/modelosfuncionalidades/NomeEmail.js
@@ -1,31 +1,57 @@
-import React, { useContext,useState } from "react";
-import NomeCompleto from "../funcionalidades/NomeCompleto";
-import { CentraldeDadosProvider,  CentraldeDadosContext  } from "../CentralDeDadosContext";
-import {Email} from "../funcionalidades/Email";
+import React, { useContext, useState } from "react";
+import {CentraldeDadosContext } from "../CentralDeDadosContext";
 
 function NomeEmail() {
     const { dadosEmail, AtualizarDadosEmail } = useContext(CentraldeDadosContext);
     const [novoEmail, setNovoEmail] = useState('');
+    
+    const { dadosNome, AtualizarDadosNome } = useContext(CentraldeDadosContext);
+    const [novoNome, setNovoNome] = useState('');
 
 
-    function handleChangeEmail(event){
+    function handleChangeEmail(event) {
         setNovoEmail(event.target.value)
     }
 
-    function atualizarContexto(){
-        Email.AtualizarDadosEmail()
-        // AtualizarDadosEmail(novoEmail)
-        // console.log(novoEmail)
-        console.log()
+    function handleChangeNome(event) {
+        setNovoNome(event.target.value)
     }
 
+    function atualizarContexto() {
+        AtualizarDadosEmail(novoEmail)
+        console.log(novoEmail)
+        console.log(dadosEmail)
+        AtualizarDadosNome(novoNome)
+        console.log(novoNome)
+        console.log(dadosNome)
+    }
+
+    if (novoEmail) {
+        AtualizarDadosEmail(novoEmail)
+    }
+    if (novoNome) {
+        AtualizarDadosEmail(novoNome)
+    }
     return (
         <div className="flex justify-center items-center flex-col w-[600px] h-[400px] rounded-[40px] border-laranja border-[2px] m-auto " >
             <div className=" w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center">
-                <NomeCompleto ></NomeCompleto>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="Nome completo"
+                    onChange={handleChangeNome}
+                    value={novoNome}
+                    className=" placeholder:text-white placeholder:opacity-70 z-50 text-[25px] fonteBold w-[470px] pl-[10px] h-[60px] bg-roxo  bg-opacity-20 rounded-[17.50px] "></input>
+                </div>
             </div>
             <div className=" w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center mt-[17px]">
-                <Email handleChange={handleChangeEmail} novoEmail={novoEmail}></Email>
+                <input
+                    type="text"
+                    onChange={handleChangeEmail}
+                    value={novoEmail}
+                    placeholder="Email"
+                    className="placeholder:text-white placeholder:opacity-70 z-50 text-[25px] fonteBold w-[470px] pl-[10px] h-[60px] bg-roxo bg-opacity-20 rounded-[17.50px]"
+                />
             </div>
             <button onClick={atualizarContexto} className="w-[500px] h-[60px] text-[20px] fonteBold bg-laranja rounded-[20px] text-white mt-[30px] z-50">Solicitar agora</button>
         </div>
