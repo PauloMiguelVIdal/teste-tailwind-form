f884fcc3429e563b7dd9b8572f3bfa59613b34e4
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-01-08 04:57:38 -0300

    ajustado parte primeiro nome do código

--- src/paginaCartãoBlack/PaginaFormularioBlackResposta/PaginaFormularioBlackResposta.js
+++ src/paginaCartãoBlack/PaginaFormularioBlackResposta/PaginaFormularioBlackResposta.js
@@ -6,11 +6,11 @@ import { CentraldeDadosContext } from "../../ComponentesUniversais/CentralDeDado
 
 function PaginaFormularioBlackResposta() {
 
-    const {dadosNome } = useContext(CentraldeDadosContext);
+    const {dadosPrimeirosNome } = useContext(CentraldeDadosContext);
 
     return (
         <div className="w-full h-[1024px] bg-gradient-to-b from-roxoForte to-black">
-            <h1 className="text-laranja text-[100px] text-center fonteBold pt-[0px] relative z-10 mb-[70px]">Olá, {dadosNome}</h1>
+            <h1 className="text-laranja text-[100px] text-center fonteBold pt-[0px] relative z-10 mb-[70px]">Olá, {dadosPrimeirosNome}</h1>
             <h2 className="text-[40px] text-roxo text-center fonteBold mb-[50px]">Solicitação realizada com sucesso</h2>
             <h4 className="text-center text-[40px] text-white fonteLight">A partir de agora, nossos especialistas <br /> estarão revisando cuidadosamente seu <br /> perfil para verificar se você atende a todos <br /> os requisitos necessários. Aguardamos a <br /> oportunidade de entrar em contato via <br />e-mail dentro do prazo de 3 dias<br /> úteis, fornecendo informações adicionais<br /> sobre o processo.</h4>
             <div>
