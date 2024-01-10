23d0bf2103f317e83af8bb06aaa50d0cc1b00c79
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-01-10 05:05:26 -0300

    feito a lógica do cartão standart

--- src/paginaCartãoStandart/PaginaFormularioStandartResposta/PaginaFormularioCartãoStandartResposta.js
+++ src/paginaCartãoStandart/PaginaFormularioStandartResposta/PaginaFormularioCartãoStandartResposta.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { CentraldeDadosContext } from "../../ComponentesUniversais/CentralDeDadosContext";
 
 function PaginaFormularioCartãoStandartResposta(){
+
+    const {dadosPrimeirosNome } = useContext(CentraldeDadosContext);
+
     return(
         <div className="w-full h-[1024px] bg-gradient-to-b from-roxo to-roxoForte">
-            <h1 className="text-laranja text-[100px] text-center fonteBold pt-[0px] relative z-10 mb-[70px]">Olá, GUTO</h1>
+            <h1 className="text-laranja text-[100px] text-center fonteBold pt-[0px] relative z-10 mb-[70px]">Olá, {dadosPrimeirosNome}</h1>
             <h2 className="text-[40px] text-roxoForte text-center fonteBold mb-[50px]">Solicitação realizada com sucesso</h2>
             <h4 className="text-center text-[40px] text-white fonteLight">A partir de agora, nossos especialistas <br /> estarão revisando cuidadosamente seu <br /> perfil para verificar se você atende a todos <br /> os requisitos necessários. Aguardamos a <br /> oportunidade de entrar em contato via <br />e-mail dentro do prazo de 3 dias<br /> úteis, fornecendo informações adicionais<br /> sobre o processo.</h4>
             <div>
