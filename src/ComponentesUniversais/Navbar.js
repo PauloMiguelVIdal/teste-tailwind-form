2af1dc7d4741631683cd18260c785971bdd12b01
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-01-11 20:27:10 -0300

    iniciado as alterações na navbar

--- src/ComponentesUniversais/Navbar.js
+++ src/ComponentesUniversais/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import obcoin from "../Logo/OBCOIN svg.svg"
 function Navbar(){
     return(
-    <div className="bg-white w-full h-32 flex flex-row items-center place-content-between">
+    <div className="bg-roxo w-full h-32 flex flex-row items-center place-content-between">
         <img width={100} height={100} className="flex items-center ml-4" src={obcoin} alt="obcoin"></img>
         <ul className="flex text-laranja text-3xl w-[1200] place-content-evenly fonteBold">
             <li className="pr-12">App</li>
