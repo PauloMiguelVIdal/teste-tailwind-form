1cf691ad6056357dbdab94f87021357a2f638212
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-01-12 05:57:39 -0300

    alterado hover da navbar e iniciado criação de dropdown

--- src/ComponentesUniversais/Navbar.js
+++ src/ComponentesUniversais/Navbar.js
@@ -5,12 +5,16 @@ function Navbar(){
     <div className="bg-roxo w-full h-32 flex flex-row items-center place-content-between">
         <img width={100} height={100} className="flex items-center ml-4" src={obcoin} alt="obcoin"></img>
         <ul className="flex text-laranja text-3xl w-[1200] place-content-evenly fonteBold">
-            <li className="pr-12">App</li>
-            <li className="pr-12">Cartões</li>
-            <li className="pr-12">Benefícios</li>
-            <li className="pr-12">Moeda</li>
-            <li className="pr-12">Cultura</li>
-            <li className="pr-12">Candidatura</li>
+            <li className="navbarHover">App</li>
+            <li className="navbarHover">Cartões</li>
+            {/* <ul>
+                <li>Cartão Standart</li>
+                <li>Cartão Black</li>
+            </ul> */}
+            <li className="navbarHover">Benefícios</li>
+            <li className="navbarHover">Moeda</li>
+            <li className="navbarHover">Cultura</li>
+            <li className="navbarHover">Candidatura</li>
         </ul>
     </div>
 )
