26d9bcceca165fbbb9abaed561a98590efbcea5a
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-01-29 05:08:39 -0300

    criado base do design cartão parte traseiro

--- src/ComponentesUniversais/Cartões/CartãoBlack/costasCartãoBlackLimpo.js
+++ src/ComponentesUniversais/Cartões/CartãoBlack/costasCartãoBlackLimpo.js
@@ -6,42 +6,53 @@ function CostasCartãoBlackLimpo() {
     const { dadosNumeroCartão } = useContext(CentraldeDadosContext);
     const { dadosNumeroCVV } = useContext(CentraldeDadosContext);
     return (
-        <div>
-            <div className="origin-center flex justify-center items-center flex-row w-110 h-[37.5rem] bg-gradient-to-tl from-black from-35% to-roxoForte to-65% rounded-[40px] ml-10">
+        <div className="origin-center flex justify-center items-center flex-row w-110 h-[37.5rem] bg-gradient-to-tl from-black from-35% to-roxoForte to-65% rounded-[40px] ml-10">
+            <div className="w-[70px] h-[600px] bg-black relative z-10 mr-[130px]"></div>
 
-                <div className="w-[30px] h-[200px] bg-laranja rounded-[20px] relative bottom-[140px] left-[290px] rotate-180 z-10">
-                    <h4 className="text-white rotate-90 bg-laranja fonteBold text-[15px] relative left-[100px] bottom-[70px]" >Ouvidoria:<br />
-                        ouvidoria @OBank.com.br<br />
-                        0800 887 0463</h4>
-                </div>
-                <div>
-                    <h3 className="text-white rotate-90">Está com dúvida?<br />
-                        Nosso app póde te ajudar.</h3>
-                </div>
-                <div>
-                    <h3 className="text-white rotate-90">Fale com a gente<br />
-                        08005912117
-                        </h3>
-                </div>
-                <div>
-                    <h3 className="text-white rotate-90">Outside Brazil <br />
-                        1636 722 7111
-                    </h3>
-                </div>
-                <div className="w-[30px] h-[100px] bg-laranja rounded-[20px] relative bottom-[140px] left-[290px] rotate-180">
-                    <h4 className="text-laranja rotate-90 fonteBold text-[15px] relative left-[80px] top-[17px]" >{dadosNumeroCartão}</h4>
-                </div>
+            <div>
 
-                <div className="relative bottom-[140px] left-[290px] rotate-180">
-                    <div className="w-[27px] h-[95px] bg-laranjaFraco relative left-[29px] bottom-[6px] rounded-[20px] ">
-                        <div className="w-[32px] h-[55px] bg-roxoForte relative right-[2.5px] bott-[1px] rounded-[20px]">
-                            <h4 className="text-white rotate-90 fonteBold text-[15px] relative  top-[17px]" >cvv</h4>
+                <div className="bg-white w-[400px] h-[275px] rotate-90 flex flex-row">
+
+
+                    <div className="bg-black w-[200px] h-[275px] rotate-180 flex items-center flex-col justify-around">
+                        <div className="w-[195px] h-[55px] bg-laranja rounded-[20px]" >
+                            <h4 className="text-white  fonteBold text-[15px]" >Ouvidoria:<br />
+                                ouvidoria @OBank.com.br<br />
+                                0800 887 0463</h4>
                         </div>
-                        <h4 className="text-white rotate-90 fonteBold text-[15px] relative top-[8px]" >{dadosNumeroCVV}</h4>
+
+                        <div className="w-[30px] h-[100px] bg-laranja rounded-[20px]">
+                            <h4 className="text-laranja fonteBold text-[15px]" >{dadosNumeroCartão}</h4>
+                        </div>
+
+                        <div className="rotate-90">
+                            <div className="w-[27px] h-[95px] bg-laranjaFraco rounded-[20px] ">
+                                <div className="w-[32px] h-[55px] bg-roxoForte rounded-[20px]">
+                                    <h4 className="text-white  fonteBold text-[15px] " >cvv</h4>
+                                </div>
+                                <h4 className="text-white fonteBold text-[15px]" >{dadosNumeroCVV}</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-black flex items-center flex-col w-[200px] h-[275px] justify-around ">
+                        <div className="w-[195px] h-[55px] bg-laranja rounded-[20px] bottom-[0px] left-[0px] rotate-180 ">
+                            <h3 className="text-white">Está com dúvida?<br />
+                                Nosso app póde te ajudar.</h3>
+                        </div>
+                        <div className="w-[195px] h-[55px] bg-laranja rounded-[20px] bottom-[0px] left-[0px] rotate-180 ">
+                            <h3 className="text-white">Fale com a gente<br />
+                                08005912117
+                            </h3>
+                        </div>
+                        <div className="w-[195px] h-[55px] bg-laranja rounded-[20px] bottom-[0px] left-[0px] rotate-180 ">
+                            <h3 className="text-white">Outside Brazil <br />
+                                1636 722 7111
+                            </h3>
+                        </div>
                     </div>
                 </div>
 
-                <div className="w-[70px] h-[600px] bg-black relative z-10 mr-[130px]"></div>
+                {/* parte do cartção black */}
                 <div className="w-[87px] h-16 origin-top-left top-[280px] right-[50px] rotate-[-91.79deg] z-10 relative bg-neutral-600 rounded-full">
                     <div className="w-[73px] h-16 origin-top-left z-20 relative bg-gradient-to-b from-black to-white rounded-full"></div>
                 </div>
