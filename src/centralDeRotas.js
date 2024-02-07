b5cc88b31b293de2d9c16633b61f9f4dcb83cf11
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-02-07 07:26:12 -0300

    criando página cartões

---
+++ src/centralDeRotas.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import App from "./App";
+// import { render } from "@testing-library/react";
+// import { BrowserRouter } from "react-router-dom";
+// render(
+//     <BrowserRouter>
+//     <App/>
+//     </BrowserRouter>
+//     document.getElementById('root')
+// )
