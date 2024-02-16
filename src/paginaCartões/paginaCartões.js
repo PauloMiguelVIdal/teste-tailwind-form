8239b480db06c8ee046b3ab37cad4648f4e633e3
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-02-15 22:20:46 -0300

    adicionado  comoentários

--- src/paginaCartões/paginaCartões.js
+++ src/paginaCartões/paginaCartões.js
@@ -4,15 +4,25 @@ import PaginaEscolhaSeuCartão from "./PaginaEscolhaSeuCartão";
 import PaginaPlanosCartões from "./PaginaPlanosCartões";
 import Footer from "../ComponentesUniversais/Footer";
 
+const scrollParaFormulárioBlack = () => {
+    const formulario = document.getElementById("formularioCartaoBlack");
+    if (formulario) {
+        window.scrollTo({
+            behavior: 'smooth',
+            top: formulario.offsetTop
+        });
+    }
+};
 
 
  function PaginaCartões(){
 
     return(
         <div>
-            <PaginaBemVindoCartões />
+            
+            <PaginaBemVindoCartões />{/*pagina na qual está a navbar e os componentes inicias  */}
             <PaginaEscolhaSeuCartão />
-            <PaginaPlanosCartões />
+            <PaginaPlanosCartões  scrollParaFormulário={scrollParaFormulárioBlack} />
             <Footer/>
         </div>
     )
