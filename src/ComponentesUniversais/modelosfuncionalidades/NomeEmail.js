bdf4b54cf50ac67ca88841e5d1f86eee0c994400
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-02-26 06:06:50 -0300

    alterado a forma como se cria o cartão

--- src/ComponentesUniversais/modelosfuncionalidades/NomeEmail.js
+++ src/ComponentesUniversais/modelosfuncionalidades/NomeEmail.js
@@ -2,84 +2,53 @@ import React, { useContext, useState } from "react";
 import { CentraldeDadosContext } from "../CentralDeDadosContext";
 
 function NomeEmail() {
-    const { dadosEmail, AtualizarDadosEmail } = useContext(CentraldeDadosContext);
-    const [novoEmail, setNovoEmail] = useState('');
+    const { dadosEmail, AtualizarDadosEmail, 
+            dadosNome, AtualizarDadosNome, 
+            dadosNumeroCartão, AtualizarDadosNumerocartão, 
+            dadosNumeroCVV, AtualizarDadosNumeroCVV } = useContext(CentraldeDadosContext);
 
-    const { dadosNome, AtualizarDadosNome } = useContext(CentraldeDadosContext);
+    const [novoEmail, setNovoEmail] = useState('');
     const [novoNome, setNovoNome] = useState('');
 
-
-    const { dadosNumeroCartão, AtualizarDadosNumerocartão } = useContext(CentraldeDadosContext);
-    const [novoNumeroCartão, setNovoNumeroCartão] = useState('');
-
-    const { dadosNumeroCVV, AtualizarDadosNumeroCVV } = useContext(CentraldeDadosContext);
-    const [novoNumeroCVV, setNovoNumeroCVV] = useState('');
-
-
-
-
     function handleChangeEmail(event) {
-        setNovoEmail(event.target.value)
+        setNovoEmail(event.target.value);
     }
 
     function handleChangeNome(event) {
-        const novoValor = event.target.value || "";
-        setNovoNome(novoValor.toUpperCase())
+        setNovoNome(event.target.value.toUpperCase());
+    }
+
+    function gerarNumeroCartao() {
+        return String(Math.random(22)).slice(2);
+    }
+
+    function gerarNumeroCVV() {
+        return String(Math.floor(Math.random() * 1000));
     }
 
     function atualizarContexto() {
-
-        function geradorCartão() {
-            let numeroCartão = String(Math.random(22)).slice(2)
-            let numeroCVV = String(Math.floor(Math.random() * 1000));
-            setNovoNumeroCartão(numeroCartão)
-            setNovoNumeroCVV(numeroCVV)
-            console.log(numeroCartão)
-            console.log(numeroCVV)
+        if (novoEmail && novoNome) {
+            const novoNumeroCartão = gerarNumeroCartao();
+            const novoNumeroCVV = gerarNumeroCVV();
+            AtualizarDadosEmail(novoEmail);
+            AtualizarDadosNome(novoNome);
+            AtualizarDadosNumerocartão(novoNumeroCartão);
+            AtualizarDadosNumeroCVV(novoNumeroCVV);
         }
-        geradorCartão()
-
-        if (novoEmail) {
-            AtualizarDadosEmail(novoEmail)
-        }
-        if (novoNome) {
-            AtualizarDadosNome(novoNome)
-            AtualizarDadosNumerocartão(novoNumeroCartão)
-            AtualizarDadosNumeroCVV(novoNumeroCVV)
-
-        }
-
-
-
-        console.log(novoEmail)
-        console.log(dadosEmail)
-
-        console.log(novoNome)
-        console.log(dadosNome)
-
-        console.log(novoNumeroCartão)
-        console.log(dadosNumeroCartão)
-
-        console.log(novoNumeroCVV)
-        console.log(dadosNumeroCVV)
-
-
     }
 
-
     return (
-        <div className="flex justify-center items-center flex-col w-[600px] h-[400px] rounded-[40px] border-laranja border-[2px] m-auto " >
-            <div className=" w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center">
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Nome completo"
-                        onChange={handleChangeNome}
-                        value={novoNome}
-                        className=" placeholder:text-white placeholder:opacity-70 z-50 text-[25px] fonteBold w-[470px] pl-[10px] h-[60px] bg-roxo  bg-opacity-20 rounded-[17.50px] "></input>
-                </div>
+        <div className="flex justify-center items-center flex-col w-[600px] h-[400px] rounded-[40px] border-laranja border-[2px] m-auto">
+            <div className="w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center">
+                <input
+                    type="text"
+                    placeholder="Nome completo"
+                    onChange={handleChangeNome}
+                    value={novoNome}
+                    className="placeholder:text-white placeholder:opacity-70 z-50 text-[25px] fonteBold w-[470px] pl-[10px] h-[60px] bg-roxo bg-opacity-20 rounded-[17.50px]"
+                />
             </div>
-            <div className=" w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center mt-[17px]">
+            <div className="w-[500px] h-[80px] rounded-[27.50px] text-white bg-roxo bg-opacity-40 flex justify-center items-center mt-[17px]">
                 <input
                     type="text"
                     onChange={handleChangeEmail}
@@ -93,5 +62,4 @@ function NomeEmail() {
     )
 }
 
-export default NomeEmail
-
+export default NomeEmail;
