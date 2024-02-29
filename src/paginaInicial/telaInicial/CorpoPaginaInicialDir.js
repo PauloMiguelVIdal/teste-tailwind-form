e860df726194c795a7d1c7b765c8dae18f799adf
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-02-29 06:12:04 -0300

    alterado p´´agina inicial

--- src/paginaInicial/telaInicial/CorpoPaginaInicialDir.js
+++ src/paginaInicial/telaInicial/CorpoPaginaInicialDir.js
@@ -4,7 +4,7 @@ function CorpoPaginaInicialDir(){
 return(
     <div className="">        
 
-        <div className="border-white/20 border-[3px] rounded-tl-[20px] w-[42.76rem] bg-transparent relative overflow-hidden h-[54.6rem] top-6 left-[210px] z-40">
+        {/* <div className="border-white/20 border-[3px] rounded-tl-[20px] w-[42.76rem] bg-transparent relative overflow-hidden h-[54.6rem] top-6 left-[210px] z-40">
         <div className="border-white/30 border-[3px] rounded-tl-[20px] w-[52.1rem] bg-transparent relative overflow-hidden h-[52.6rem] top-8 left-8 z-30">
         <div className="border-white/40 border-[3px] rounded-tl-[20px] w-[49.85rem] bg-transparent relative overflow-hidden h-[50.5rem] top-8 left-8 z-20">
         <div className="border-white/50 border-[3px] rounded-tl-[20px] w-[47.65rem] bg-transparent relative overflow-hidden h-[49.5rem] top-8 left-8">
@@ -13,7 +13,7 @@ return(
         </div>
         </div>
         </div>
-        </div>
+        </div> */}
         <img className="top-[224px] right-[100px] w-[800px] h-[800px] absolute z-50" src={mulher} alt="mulher"></img>
     </div>
 )
--- src/paginaInicial/telaInicial/CorpoPaginaInicialEsq.js
+++ src/paginaInicial/telaInicial/CorpoPaginaInicialEsq.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContainerFundamentos from "./ContainerFundamentos";
 function CorpoPaginaInicialEsq() {
     return (
-        <div className="text-center">
+        <div className="text-center relative right-[300px] top-[60px]">
             <h1 className="text-white text-5xl m-6 fonteBold"> UNINDO FINANÇAS E  <br></br>
                 TECNOLOGIA</h1>
             <ContainerFundamentos></ContainerFundamentos>
